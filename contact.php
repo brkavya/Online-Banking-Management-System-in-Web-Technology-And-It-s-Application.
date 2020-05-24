675be771454ec50f5ce55503e6330c41b9538c84
KAVYA  B R <45337776+brkavya@users.noreply.github.com>
2020-05-24 13:08:13 +0530

    please read README.md  for full details

---
+++ contact.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title>Contact Us</title>
+        
+        <link rel="stylesheet" href="newcss.css">
+        <style>
+            .heading{
+    font-weight:bold;
+    color:#2E4372;
+}
+        </style>
+       
+    </head>
+        <?php include 'header.php' ?>
+        <div class='content_customer'>
+            <h3 style="text-align:center;color:#2E4372;"><u>Contact Us</u></h3>
+            
+            <div class="contact">
+            
+             <h3 style="color:#2E4372;"><u>Bangalore Branch</u></h3>
+            <p><span class="heading">Address - </span>sy no.63,off magadi road, Bangalore-91</p>
+            <p><span class="heading">Tel - </span>8023589500</p>
+            <p><span class="heading">Email - </span>bangalorebranch@onlinebank.com</p>
+
+ <h3 style="color:#2E4372;"><u>Kolkata Branch</u></h3>
+            <p><span class="heading">Address - </span>sy no.963,off chitpur road, kolkata-11</p>
+            <p><span class="heading">Tel - </span>8023588500</p>
+            <p><span class="heading">Email - </span>kolkatabranch@onlinebank.com</p>
+
+ <h3 style="color:#2E4372;"><u>Delhi Branch</u></h3>
+            <p><span class="heading">Address - </span>sy no.63,off taj road, Delhi-91</p>
+            <p><span class="heading">Tel - </span>8023587500</p>
+            <p><span class="heading">Email - </span>delhibranch@onlinebank.com</p>
+            </div>
+            </div>
